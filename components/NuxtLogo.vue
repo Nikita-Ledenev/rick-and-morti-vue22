55c9999abcd0c03a2508a5d03e576513227050cb
Nikita-Ledenev <115636879+Nikita-Ledenev@users.noreply.github.com>
2023-07-24 21:10:23 +0300

    project new

---
+++ components/NuxtLogo.vue
@@ -0,0 +1,7 @@
+<template></template>
+
+<style>
+.nuxt-logo {
+  height: 180px;
+}
+</style>
---
+++ components/Tutorial.vue
@@ -0,0 +1,10 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class=""></div>
+</template>
+
+<script>
+export default {
+  name: "NuxtTutorial",
+};
+</script>
